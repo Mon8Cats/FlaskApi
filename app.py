f7c74afdbb2ec9ca629e989f0a70a95f8f6aa3fb
StoneRoot <stoneroot@StoneRoots-MBP.lan>
2020-12-19 14:57:49 -0600

    call db.init_app(app)

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
+db.init_app(app)
+
 '''
 default values
 the authentication endpoint: /auth
@@ -56,5 +58,5 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    db.init_app(app)
+   
     app.run(port=5000, debug=True)
